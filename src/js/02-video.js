e872b85c0241d9050fa4f8ba4ee1b5be0dd4b33c
Maryshchyn <maryshchyn89@gmail.com>
2022-08-04 19:30:02 +0300

    rte

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,26 @@
+import throttle from 'lodash.throttle';
+import Player from '@vimeo/player';
 
+
+const iframe = document.querySelector('iframe');
+const player = new Player(iframe);
+const LOCALSTORAGE_KEY = "videoplayer-current-time";
+
+
+const onPlay = function(data) {console.log('Play the video!')};
+player.on('play', onPlay);
+
+
+player.on('timeupdate', throttle(timeUpdateNow, 2000));
+function timeUpdateNow(evt) {
+    localStorage.setItem(LOCALSTORAGE_KEY, evt.seconds);
+    console.log(`Video now:  ${evt.seconds}`); 
+}
+
+
+const saveCurrentTime = localStorage.getItem(LOCALSTORAGE_KEY);
+if (saveCurrentTime) {
+    player.setCurrentTime(saveCurrentTime)
+    console.log(saveCurrentTime);
+    localStorage.removeItem(LOCALSTORAGE_KEY);
+}

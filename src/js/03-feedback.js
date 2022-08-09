f4eadedb0fbc4cbde64a52d60fa039aad9f1e1a4
Maryshchyn <maryshchyn89@gmail.com>
2022-08-09 13:48:58 +0300

    gthtdshrf

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,9 +1,6 @@
 import throttle from 'lodash.throttle';
-
 const STORAGE_KEY = 'feedback-form-state';
-
 const formData = {};
-
 const refs = {
     form: document.querySelector('.feedback-form'),
     textarea: document.querySelector('.feedback-form textarea'),
@@ -14,11 +11,17 @@ refs.textarea.addEventListener('input', throttle(onTextareaInput, 500));
 
 function onForSubmit(e) {
     e.preventDefault();
-    console.log('YES')
-
+    const mailForConsole = document.querySelector('[name="email"]').value;
+    const messagaForConsole = document.querySelector('[name="message"]').value;
+    
+    console.log(mailForConsole);
+    console.log(messagaForConsole);
+    
+    
     e.currentTarget.reset();
     localStorage.removeItem(STORAGE_KEY);
 };
+
  
 function onTextareaInput(e) {
     const message = e.target.value;
@@ -27,18 +30,28 @@ function onTextareaInput(e) {
 
 function populateTextarea() {
     const savedMessage = localStorage.getItem(STORAGE_KEY);
-    if (savedMessage) {
-        console.log(savedMessage);
-        refs.textarea.value = savedMessage;
-        
-}
+    // if (savedMessage) {
+    //     console.log(savedMessage);
+    // };
     
 };
-
 populateTextarea();
 refs.form.addEventListener('input', e => {
     formData[e.target.name] = e.target.value;
 localStorage.setItem(STORAGE_KEY, JSON.stringify(formData));
     console.log(formData);
 });
+
+// const qwe = Object.entries(mailForConsole);
+// console.log(qwe)
+// qwe@mail.com
+
+
+
+
+
+
+
+
+
 

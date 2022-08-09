1b5b531b853cea401219f679acbb1a19f9c81f7f
Maryshchyn <maryshchyn89@gmail.com>
2022-08-09 15:46:31 +0300

    erere

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,57 +1,48 @@
 import throttle from 'lodash.throttle';
-const STORAGE_KEY = 'feedback-form-state';
-const formData = {};
-const refs = {
-    form: document.querySelector('.feedback-form'),
-    textarea: document.querySelector('.feedback-form textarea'),
-};
 
-refs.form.addEventListener('submit', onForSubmit);
-refs.textarea.addEventListener('input', throttle(onTextareaInput, 500));
+const FEEDBACK_FORM_KEY = 'feedback-form-state';
+const formRef = document.querySelector('.feedback-form');
 
-function onForSubmit(e) {
-    e.preventDefault();
-    const mailForConsole = document.querySelector('[name="email"]').value;
-    const messagaForConsole = document.querySelector('[name="message"]').value;
-    
-    console.log(mailForConsole);
-    console.log(messagaForConsole);
-    
-    
-    e.currentTarget.reset();
-    localStorage.removeItem(STORAGE_KEY);
-};
+initForm();
 
- 
-function onTextareaInput(e) {
-    const message = e.target.value;
-    localStorage.setItem(STORAGE_KEY, message);
-};
+formRef.addEventListener('input', throttle(onInputForm, 500));
 
-function populateTextarea() {
-    const savedMessage = localStorage.getItem(STORAGE_KEY);
-    // if (savedMessage) {
-    //     console.log(savedMessage);
-    // };
-    
-};
-populateTextarea();
-refs.form.addEventListener('input', e => {
-    formData[e.target.name] = e.target.value;
-localStorage.setItem(STORAGE_KEY, JSON.stringify(formData));
-    console.log(formData);
+formRef.addEventListener('submit', e => {
+  e.preventDefault();
+
+  if (
+    e.target.elements.email.value !== '' &&
+    e.target.elements.message.value !== ''
+  ) {
+    const formData = new FormData(formRef);
+
+    formData.forEach((value, name) => console.log(`${name}: ${value}`));
+
+    localStorage.removeItem(FEEDBACK_FORM_KEY);
+    formRef.reset();
+  } else {
+    alert('need to fiil all fields');
+  }
 });
 
-// const qwe = Object.entries(mailForConsole);
-// console.log(qwe)
-// qwe@mail.com
+function onInputForm(e) {
+  let savedForm = localStorage.getItem(FEEDBACK_FORM_KEY);
 
+  savedForm = savedForm ? JSON.parse(savedForm) : {};
 
+  savedForm[e.target.name] = e.target.value;
 
+  localStorage.setItem(FEEDBACK_FORM_KEY, JSON.stringify(savedForm));
+}
 
+function initForm() {
+  let savedForm = localStorage.getItem(FEEDBACK_FORM_KEY);
 
+  if (savedForm) {
+    savedForm = JSON.parse(savedForm);
 
-
-
-
-
+    Object.entries(savedForm).forEach(([name, value]) => {
+      formRef.elements[name].value = value;
+    });
+  }
+}
